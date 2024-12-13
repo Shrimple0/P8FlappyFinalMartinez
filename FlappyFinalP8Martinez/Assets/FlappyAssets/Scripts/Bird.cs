f6d12e80da1e5f92c9bb1c9983a23c003883e400
Shrimple0 <756116@student.dusd.net>
2024-12-13 15:23:28 -0800

    programming background

--- FlappyFinalP8Martinez/Assets/FlappyAssets/Scripts/Bird.cs
+++ FlappyFinalP8Martinez/Assets/FlappyAssets/Scripts/Bird.cs
@@ -33,7 +33,9 @@ public class Bird : MonoBehaviour
 
     private void OnCollisionEnter2D ()
     {
+        rb2d.velocity = Vector2.zero;
         isDead = true;
         anim.SetTrigger("Die");
+        GameControl.Instance.BirdDied();
     }   
 }
--- FlappyFinalP8Martinez/Assets/FlappyAssets/Scripts/GameControl.cs
+++ FlappyFinalP8Martinez/Assets/FlappyAssets/Scripts/GameControl.cs
@@ -1,26 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameControl : MonoBehaviour { 
 
+    public static GameControl Instance;
     public GameObject GameOverText;
-
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameOver == true && Input.GetMouseButtonDown (0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 
     public void BirdDied()
     {
         GameOverText.SetActive(true);
+        gameOver = true;
     }
 }
